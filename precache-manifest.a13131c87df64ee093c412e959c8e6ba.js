c24b738732199180c732ad98b7fd07430245e5b8
Atif Farooq <u6104602@anu.edu.au>
2019-04-12 21:16:57 +1000

    Updates

---
+++ precache-manifest.a13131c87df64ee093c412e959c8e6ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "da25e9fb37d5f38a5139",
+    "url": "/react-lyricfinder-gh-pages/static/js/runtime~main.1c877b32.js"
+  },
+  {
+    "revision": "f08724299b908b78bd92",
+    "url": "/react-lyricfinder-gh-pages/static/js/main.99ebf122.chunk.js"
+  },
+  {
+    "revision": "ffb9355a35967f57110e",
+    "url": "/react-lyricfinder-gh-pages/static/js/2.5f1e79b8.chunk.js"
+  },
+  {
+    "revision": "81d9e4af3cf50881d31d7735986bf5be",
+    "url": "/react-lyricfinder-gh-pages/index.html"
+  }
+];
